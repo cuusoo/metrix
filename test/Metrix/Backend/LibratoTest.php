ab2e067df57dbbe32e0a0f96ffc6372a62e33e93
Richard Ramsden <richard@rramsden.ca>
2013-02-26 15:34:53 +0900

    Add tests to assert increment and decrement work

--- test/Metrix/Backend/LibratoTest.php
+++ test/Metrix/Backend/LibratoTest.php
@@ -1,11 +1,36 @@
 <?php
 require_once realpath(__DIR__.'/../../../Metrix.php');
+require_once 'HTTP/Request2/Response.php';
+
+use \HTTP_Request2_Response as HttpResponse;
+
+class MockHTTPClient {
+    public $body;
+    public $sent;
+    public $header;
+    public $response;
+
+    public function setAuth($a, $b, $c) { }
+    public function setHeader($arr) { }
+    public function setMethod($method) { }
+    public function setBody($body) {
+        $this->body = $body;
+    }
+    public function send() {
+        return $this->response;
+    }
+}
 
 class LibratoTest extends PHPUnit_Framework_TestCase {
     protected $client;
     protected $mockHttpClient;
 
+    //
+    // TEST SETUP
+    //
+
     public function setUp() {
+        ////
         // Setup Librato Instance
         $this->client = new Metrix(array(
             'backend' => 'librato',
@@ -14,19 +39,58 @@ class LibratoTest extends PHPUnit_Framework_TestCase {
                 'token' => '123'
             )
         ));
+
+        ////
+        // Mock out HTTPClient
+        $mocked = new MockHTTPClient();
+        $mocked->response = new HttpResponse("HTTP/1.1 200 OK");
+        $this->mockHttpClient = $mocked;
+        $this->client->getBackend()->setHttpClient($this->mockHttpClient);
     }
+
+    //
+    // TEST CLEANUP
+    //
 
     public function tearDown() {
         unset($this->client);
+        unset($this->mockHttpClient);
     }
 
-    public function testReturnsErrorOnBadHTTPCode() {
-        $mock = $this->getMock("\HTTP_Request2");
-        $mock->expects($this->any())
-             ->method('send')
-             ->will($this->returnValue(''));
-        $this->client->getBackend()->setHttpClient($mock);
+    //
+    // SIMPLE INTERFACE TESTS
+    //
+
+    public function testIncrement() {
+        // passing a single key
+        $expected = "{\"counters\":{\"key\":{\"value\":1}}}";
+        $this->client->increment('key');
+        $this->assertEquals($expected, $this->mockHttpClient->body);
+
+        // passing multiple keys
+        $expected = '{"counters":{"key1":{"value":1},"key2":{"value":1}}}';
+        $this->client->increment(array('key1', 'key2'));
+        $this->assertEquals($expected, $this->mockHttpClient->body);
     }
+
+    public function testDecrement() {
+        $expected = "Librato only supports absolute counters";
+        try {
+            $this->client->decrement('key');
+        } catch (Exception $e) {
+            $this->assertEquals($expected, $e->getMessage());
+        }
+    }
+
+    public function testGauge() {
+        $expected = "{\"gauges\":{\"key\":{\"value\":10}}}";
+        $this->client->gauge('key', 10);
+        $this->assertEquals($expected, $this->mockHttpClient->body);
+    }
+
+    //
+    // TEST EXCEPTIONAL ERRORS
+    //
 
     public function testMissingEmailParam() {
         $expected = 'Librato requires `email` parameter';
